028c73b0a90a73ed214988c17b521ebcc612872a
maverick-farhan <mdfarhanofficial@gmail.com>
2024-03-04 13:30:50 +0530

    Added

---
+++ README.md
@@ -0,0 +1,6 @@
+# Newsletter in PHP HTML CSS3 and JS
+
+![image](./newsletter.png);
+![image](./newsletter_not_valid.png);
+![image](./newsletter_empty.png);
+![image](./newsletter_db.png);
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+#hostname = server_host like localhost
+#username = your db username
+#password = your db password
+#database = your database name
+$connect = mysqli_connect("hostname", "username", "password", "database") 
+or die("Failed to connect ");
