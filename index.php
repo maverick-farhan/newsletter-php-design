8906a3f22590bd2566e2db28fde03d16e6f26682
maverick-farhan <mdfarhanofficial@gmail.com>
2024-03-04 11:50:42 +0530

    Updated

--- index.php
+++ index.php
@@ -14,10 +14,12 @@
     <h1 class="title">Stay Updated</h1>
     <form method="post" action="<?php $_SERVER['PHP_SELF']?>">
     <div class="email-container">
-        <input type="email" name="email" id="email" />
-        <button type="submit">Subscribe</button>
+        <input type="email" name="email" id="email" required/>
+        <button type="submit" id="submit">Subscribe</button>
     </div>
+   <div class="message_div"></div>
     </form>
    </div> 
+   <script src="./main.js"></script>
 </body>
 </html>
---
+++ main.js
@@ -0,0 +1,57 @@
+// Email Validation
+//Declarative approach, "Just write, Don't ask why. No How only What" :)
+
+const email = document.querySelector("#email");
+const submit = document.querySelector('#submit');
+
+const setErrMsg = (errormsg) => {
+  const message_div = document.querySelector(".message_div");
+  message_div.className = "message_div errormsg";
+  message_div.style.visibility = "visible";
+  message_div.style.transition = "all 1s linear";
+  message_div.innerText = errormsg;
+};
+const setSuccessMsg = (successmsg) => {
+  const message_div = document.querySelector(".message_div");
+  message_div.className = "message_div successmsg";
+  message_div.style.visibility = "visible"; 
+  message_div.style.transition = "all 1s linear";
+  
+  message_div.innerText = successmsg;
+};  
+
+const isEmail = (emailVal) => {
+    var atSymbol = emailVal.indexOf("@");
+    if (atSymbol < 1) {
+      return false;
+    }  
+    var dot = emailVal.lastIndexOf(".");
+    if (dot <= atSymbol + 2) {
+      return false;
+    }
+    if (dot === email.length - 1) {
+      return false;
+    } else return true;
+  };
+
+const validateEmail = ()=>{
+const emailVal = email.value.trim();
+if(emailVal===""){
+  setErrMsg("Email Can't Be Empty"); 
+  
+}
+else if(!isEmail(emailVal)){
+  setErrMsg("Not a valid Email")
+}
+else if(emailVal<=2){
+  setErrMsg("Email must be atleast 3 characters")
+}
+else{
+  setSuccessMsg("Successfully subscribed");
+}
+}
+submit.addEventListener('click',(e)=>{
+  e.preventDefault();
+  validateEmail();
+})
+
